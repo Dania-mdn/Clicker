c63f7e45f597173eea999e9dc50a8b67798fcbac
Dania-mdn <58430901+Dania-mdn@users.noreply.github.com>
2023-02-03 16:15:57 +0200

    Refactoring V0.7
    loading...

--- Clicker/Assets/Script/Upgrade.cs
+++ Clicker/Assets/Script/Upgrade.cs
@@ -17,20 +17,20 @@ public abstract class Upgrade: MonoBehaviour
     private int _valueUpgradeClick;
     public int DefaultValueUpgradePassiv;
     private int _ValueUpgradePassive;
-    private int i;
-    public int ID;
-    private string modul;
+    private int _numberShip;
+    public int IdModul;
+    private string _modul;
     public string[] stringmodul;
-    private bool is_dostup = false;
+    private bool _available = false;
     public virtual void Start()
     {
         _audioSource = GetComponent<AudioSource>();
-        PlayerPrefs.DeleteKey((ID / 10).ToString("0"));
+        PlayerPrefs.DeleteKey((IdModul / 10).ToString("0"));
 
         //добавление характеристик
-        if (PlayerPrefs.HasKey("tap_income" + ID))
+        if (PlayerPrefs.HasKey("tap_income" + IdModul))
         {
-            _valueUpgradeClick = PlayerPrefs.GetInt("tap_income" + ID);
+            _valueUpgradeClick = PlayerPrefs.GetInt("tap_income" + IdModul);
             UpgradeClick.text = "+ " + _valueUpgradeClick.ToString("0") + "%";
         }
         else
@@ -40,9 +40,9 @@ public abstract class Upgrade: MonoBehaviour
             UpgradeClick.text = "+ " + _valueUpgradeClick.ToString("0") + "%";
         }
 
-        if (PlayerPrefs.HasKey("pass_income" + ID))
+        if (PlayerPrefs.HasKey("pass_income" + IdModul))
         {
-            _ValueUpgradePassive = PlayerPrefs.GetInt("pass_income" + ID);
+            _ValueUpgradePassive = PlayerPrefs.GetInt("pass_income" + IdModul);
             UpgradePassive.text = "+ " + _ValueUpgradePassive.ToString("0") + "%";
         }
         else
@@ -55,14 +55,14 @@ public abstract class Upgrade: MonoBehaviour
     public virtual void Update()
     {
         //добавление i
-        if (PlayerPrefs.HasKey("i" + ID + PlayerPrefs.GetInt("num_ship")))
+        if (PlayerPrefs.HasKey("i" + IdModul + PlayerPrefs.GetInt("num_ship")))
         {
-            i = PlayerPrefs.GetInt("i" + ID + PlayerPrefs.GetInt("num_ship"));
+            _numberShip = PlayerPrefs.GetInt("i" + IdModul + PlayerPrefs.GetInt("num_ship"));
         }
         else
         {
-            PlayerPrefs.SetInt("i" + ID + PlayerPrefs.GetInt("num_ship"), 0);
-            i = 0;
+            PlayerPrefs.SetInt("i" + IdModul + PlayerPrefs.GetInt("num_ship"), 0);
+            _numberShip = 0;
         }
         //присвоение цен и параметров в зависимости от выбранного корабля
         if (PlayerPrefs.HasKey("num_ship"))
@@ -70,65 +70,65 @@ public abstract class Upgrade: MonoBehaviour
             if (PlayerPrefs.GetInt("num_ship") == 0)
             {
                 _price = GameManager.price_0;
-                modul = stringmodul[0];
+                _modul = stringmodul[0];
             }
             else if (PlayerPrefs.GetInt("num_ship") == 1)
             {
                 _price = GameManager.price_1;
-                modul = stringmodul[1];
+                _modul = stringmodul[1];
             }
             else if (PlayerPrefs.GetInt("num_ship") == 2)
             {
                 _price = GameManager.price_2;
-                modul = stringmodul[2];
+                _modul = stringmodul[2];
             }
             else if (PlayerPrefs.GetInt("num_ship") == 3)
             {
                 _price = GameManager.price_3;
-                modul = stringmodul[3];
+                _modul = stringmodul[3];
             }
             else if (PlayerPrefs.GetInt("num_ship") == 4)
             {
                 _price = GameManager.price_4;
-                modul = stringmodul[4];
+                _modul = stringmodul[4];
             }
             else if (PlayerPrefs.GetInt("num_ship") == 5)
             {
                 _price = GameManager.price_5;
-                modul = stringmodul[5];
+                _modul = stringmodul[5];
             }
             else if (PlayerPrefs.GetInt("num_ship") == 6)
             {
                 _price = GameManager.price_6;
-                modul = stringmodul[6];
+                _modul = stringmodul[6];
             }
         }
         else
         {
             _price = GameManager.price_0;
-            modul = stringmodul[0];
+            _modul = stringmodul[0];
         }
 
-        if (i < _price.Length)
+        if (_numberShip < _price.Length)
         {
-            if (_price[i] < PlayerPrefs.GetFloat("Money_box"))
+            if (_price[_numberShip] < PlayerPrefs.GetFloat("Money_box"))
             {
                 ButtonSprite.color = new Color(0f, 1f, 0.168f);
-                PriceButton.text = _price[i].ToString("0");
-                if (is_dostup == false)
+                PriceButton.text = _price[_numberShip].ToString("0");
+                if (_available == false)
                 {
-                    PlayerPrefs.SetInt((ID / 10).ToString("0"), PlayerPrefs.GetInt((ID / 10).ToString("0")) + 1);
-                    is_dostup = true;
+                    PlayerPrefs.SetInt((IdModul / 10).ToString("0"), PlayerPrefs.GetInt((IdModul / 10).ToString("0")) + 1);
+                    _available = true;
                 }
             }
             else
             {
                 ButtonSprite.color = new Color(1f, 1f, 1f);
-                PriceButton.text = _price[i].ToString("0");
-                if (is_dostup == true)
+                PriceButton.text = _price[_numberShip].ToString("0");
+                if (_available == true)
                 {
-                    PlayerPrefs.SetInt((ID / 10).ToString("0"), PlayerPrefs.GetInt((ID / 10).ToString("0")) - 1);
-                    is_dostup = false;
+                    PlayerPrefs.SetInt((IdModul / 10).ToString("0"), PlayerPrefs.GetInt((IdModul / 10).ToString("0")) - 1);
+                    _available = false;
                 }
             }
         }
@@ -138,16 +138,16 @@ public abstract class Upgrade: MonoBehaviour
             PriceButton.text = "MAX".ToString();
             UpgradeClick.text = "MAX".ToString();
             UpgradePassive.text = "MAX".ToString();
-            if (is_dostup == true)
+            if (_available == true)
             {
-                PlayerPrefs.SetInt((ID / 10).ToString("0"), PlayerPrefs.GetInt((ID / 10).ToString("0")) - 1);
-                is_dostup = false;
+                PlayerPrefs.SetInt((IdModul / 10).ToString("0"), PlayerPrefs.GetInt((IdModul / 10).ToString("0")) - 1);
+                _available = false;
             }
         }
-        LevelUpgrade.text = "lvl " + PlayerPrefs.GetInt("i" + ID + PlayerPrefs.GetInt("num_ship")).ToString("0");
-        if (PlayerPrefs.HasKey("slider" + ID + PlayerPrefs.GetInt("num_ship")))
+        LevelUpgrade.text = "lvl " + PlayerPrefs.GetInt("i" + IdModul + PlayerPrefs.GetInt("num_ship")).ToString("0");
+        if (PlayerPrefs.HasKey("slider" + IdModul + PlayerPrefs.GetInt("num_ship")))
         {
-            ProgressSlider.value = PlayerPrefs.GetFloat("slider" + ID + PlayerPrefs.GetInt("num_ship"));
+            ProgressSlider.value = PlayerPrefs.GetFloat("slider" + IdModul + PlayerPrefs.GetInt("num_ship"));
             if (ProgressSlider.value > 0.9)
             {
                 ProgressSlider.value = 0;
@@ -160,46 +160,46 @@ public abstract class Upgrade: MonoBehaviour
     }
     public virtual void income()
     {
-        if (i < _price.Length && _price[i] < PlayerPrefs.GetFloat("Money_box"))
+        if (_numberShip < _price.Length && _price[_numberShip] < PlayerPrefs.GetFloat("Money_box"))
         {
             _audioSource.Play();
             //забрать денег
-            PlayerPrefs.SetFloat("Money_box", PlayerPrefs.GetFloat("Money_box") - _price[i]);
+            PlayerPrefs.SetFloat("Money_box", PlayerPrefs.GetFloat("Money_box") - _price[_numberShip]);
             //добавить характеристик
 
             //1
             _valueUpgradeClick = _valueUpgradeClick + DefaultValueUpgradeClick;
             GameManager.tap_income = GameManager.tap_income + (DefaultValueUpgradeClick * 0.01f);
-            PlayerPrefs.SetInt("tap_income" + ID, _valueUpgradeClick);
+            PlayerPrefs.SetInt("tap_income" + IdModul, _valueUpgradeClick);
             UpgradeClick.text = "+ " + _valueUpgradeClick.ToString("0") + "%";
 
             //1
             _ValueUpgradePassive = _ValueUpgradePassive + DefaultValueUpgradePassiv;
             GameManager.pass_income = GameManager.pass_income + (DefaultValueUpgradePassiv * 0.01f);
-            PlayerPrefs.SetInt("pass_income" + ID, _ValueUpgradePassive);
+            PlayerPrefs.SetInt("pass_income" + IdModul, _ValueUpgradePassive);
             UpgradePassive.text = "+ " + _ValueUpgradePassive.ToString("0") + "%";
 
             //сохранить изменения
             PlayerPrefs.SetFloat("tap_income", GameManager.tap_income);
             PlayerPrefs.SetFloat("pass_income", GameManager.pass_income);
             //перейти на новый уровень цены
-            i = i + 1;
-            if (modul != null && i % 5 == 0)
+            _numberShip = _numberShip + 1;
+            if (_modul != null && _numberShip % 5 == 0)
             {
                 int number;
-                number = i / 5;
-                PlayerPrefs.SetFloat("i_" + modul, 0);
-                PlayerPrefs.SetInt(modul, number);
+                number = _numberShip / 5;
+                PlayerPrefs.SetFloat("i_" + _modul, 0);
+                PlayerPrefs.SetInt(_modul, number);
             }
-            PlayerPrefs.SetInt("i" + ID + PlayerPrefs.GetInt("num_ship"), PlayerPrefs.GetInt("i" + ID + PlayerPrefs.GetInt("num_ship")) + 1);
+            PlayerPrefs.SetInt("i" + IdModul + PlayerPrefs.GetInt("num_ship"), PlayerPrefs.GetInt("i" + IdModul + PlayerPrefs.GetInt("num_ship")) + 1);
             ProgressSlider.value = ProgressSlider.value + 0.2f;
-            PlayerPrefs.SetFloat("slider" + ID + PlayerPrefs.GetInt("num_ship"), ProgressSlider.value);
+            PlayerPrefs.SetFloat("slider" + IdModul + PlayerPrefs.GetInt("num_ship"), ProgressSlider.value);
         }
-        if (i >= _price.Length)
+        if (_numberShip >= _price.Length)
         {
-            if (ID == 21 || ID == 22 || ID == 14)
+            if (IdModul == 21 || IdModul == 22 || IdModul == 14)
             {
-                PlayerPrefs.SetInt("Unloc " + ID, 1);
+                PlayerPrefs.SetInt("Unloc " + IdModul, 1);
             }
         }
     }
